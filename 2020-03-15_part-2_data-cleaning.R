a2c762b0fa8a415781965874f1822a7181e34569
danielsnyder116 <danielsnyder116@gmail.com>
2020-03-16 23:20:40 -0400

    Finished part 2 and started visualizations

--- 2020-03-15_part-2_data-cleaning.R
+++ 2020-03-15_part-2_data-cleaning.R
@@ -2,6 +2,7 @@ library(stringr)
 library(dplyr)
 library(tidyr)
 library(readr)
+library(ggplot2)
 
 setwd("C:/Users/602770/Downloads/volunteer/wec/Education-Impact/Data/Processed")
 
@@ -25,37 +26,62 @@ df <- df %>% separate(semester, c("semester", "year"), sep=" ", extra="merge")
 #Class Name
 df <- df %>% mutate(class_name = str_replace_all(class_name, pattern="Class Name: ", replacement = ""))
 
-# Exam Scores, Multiple Columns
-df$final_exam_score <- parse_number(df$final_exam_score)
-df$attendance <- parse_number(df$attendance)
 
-df$participation <- parse_number(df$participation)
-df$language_score <- parse_number(df$language_score)
-
-df$total_eval_score <- parse_number(df$total_eval_score)
+#Exam scores, Multiple columns NEW WAY
+#Columns you want to mutate (must be list of strings) and function(s)
+df <- df %>% mutate_at(c("final_exam_score", "participation", "attendance",
+                         "language_score", "total_eval_score"), funs(parse_number(.)))
 
 
+#Filling in all NAs with zero since that is the effective score
+#Mutate all applies the function to all columns rather than having to specify
+#Funs stands for function and is a way to call a different function on the data
+
+#May want to wait though as there may be lack of reporting, so NA isn't 
+# necessarily a zero
+#df <- mutate_all(df, funs(replace_na(., 0)))
 
 #Calculating the number of points given based on the final exam score (out of 100 score to 5 point scale)
 df$exam_points <- df$total_eval_score - (df$attendance + df$participation + df$language_score)
+
 
 #Reordering to make columns easier to read
 df <- df %>% select(student_id, last_name, first_name, semester, year, class_name, final_exam_score,
                     exam_points, attendance, participation, language_score, total_eval_score)
 
+glimpse(df)
+
+#Adding "2017 2" and "2018 2" data to 2017 and 2018 respectively
+#They were just special classes held for different training during same semester
+df <- df %>% mutate(year = str_trunc(year, width=4, ellipsis = ""))
+
+
+#Getting rid of a few rows of 90% NA data and thus unusable
+df <- df %>% filter(!is.na(year))
+
+###################### Visualizing Data (EDA) ###########################
+
+#Attendance distribution from 2015-2019 overall
+p <- ggplot(df, aes(x=attendance)) + geom_histogram(bins=5, binwidth = .5)
+p
+
+#Attendance grouped by Semester
+p_color <- ggplot(df, aes(x=attendance, fill=semester)) + 
+                  geom_histogram(bins=5, binwidth = .5) +
+                  facet_grid(.~ semester) + theme_light()
+p_color
+
+
+#By Year
+ggplot(df, aes(x=attendance, fill=year)) + 
+  geom_histogram(bins=5, binwidth = .5) +
+  facet_grid(.~ semester) + theme_light() + facet_grid(.~year)
 
 
 
-View(count(df, vars=total_eval_score))
-
-hist(df$total_eval_score)
 
 
-
-
-
-
-
+count(df, vars=year)
 
 
 
@@ -76,9 +102,13 @@ hist(df$total_eval_score)
 #   df <- parse_number(df$column)
 # }
   
-  
-  
-  
+# Exam Scores, Multiple Columns
+#Old way, see dplyr way below
+# df$final_exam_score <- parse_number(df$final_exam_score)
+# df$attendance <- parse_number(df$attendance)
+# df$participation <- parse_number(df$participation)
+# df$language_score <- parse_number(df$language_score)
+# df$total_eval_score <- parse_number(df$total_eval_score)
   
   
 
