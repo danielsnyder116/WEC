6a13462a1f9704b29617199081c68a2c7449413f
danielsnyder116 <danielsnyder116@gmail.com>
2020-05-31 14:08:36 -0400

    Chegando ai

--- Volunteers/volunteer-roster_cleanup.R
+++ Volunteers/volunteer-roster_cleanup.R
@@ -9,56 +9,46 @@ setwd("/Users/Daniel/Desktop/volunteer-files/")
 files <- list.files(pattern = "*.xls*", recursive=TRUE)
 
 #We are filtering out the workbooks that are not of use to us
-roster_files <- str_subset(files, pattern = "List|Log|Potential|Show|Sign|sign|Training|Tutor", negate = TRUE)
+roster_files <- str_subset(files, pattern = "List|Log|Potential|Show|Sign|sign|Training|Tutor|Tutoring", negate = TRUE)
 #str_subset is a wrapper around files[str_detect(files, pattern = "Log")]
 
 #This also works#Keep takes in list and a function to apply as a filter
 #roster_files <- files %>% keep(., str_detect(files, pattern = "List|Log|Potential|Show|Sign|sign|Training|Tutor", negate = TRUE))
-# roster <- roster_files[1]
+
 
 for (roster in roster_files) {
 
   #Better way is to simply combine sheets to avoid messy workbook issues
+  #Need to use "function (x)" to be able to set parameters of read_excel
+  #Doing this makes it unnecessary to put "path = roster" for the second argument of map()
+  print(roster)
   raw <- roster %>% excel_sheets() %>% set_names() %>%
-                   #function applied, #object
-               map(read_excel, path = roster) %>% bind_rows()
+                    map(function (x) read_excel(path = roster, skip = 1, col_types = "text")) %>% 
+                    bind_rows() %>% compact()
   
-
+  if (nrow(raw) == 0) {
+    
+  } else {
+    
+  
   #Due to poor file names need to add in some logic to ensure files are uniform in column
   #names to ensure easy binding later
+ 
+  #Gets the number of columns and creates a sequence (1,2,3,4,...)
+  #Then pastes each number to col to create unique, generic column names to rename the columns
+  #This will allow for easier and cleaner binding
+  colnames(raw) <- paste0("col", seq(ncol(raw)))
 
-  if (ncol(raw) == 4 ) {
-    colnames(raw) <- c("col1","col2", 'col3', 'col4')
-  }
-
-  else if (ncol(raw) == 3) {
-    colnames(raw) <- c("col1", "col2", "col3")
-  }
-
-  else if (ncol(raw) == 2) {
-    colnames(raw) <- c("col1", "col2")
-  }
-
-  else if (ncol(raw) == 1) {
-    colnames(raw) <- c("col1")
-  }
-
-  else {
-    #Only will need the first 5 columns for the info.
-    raw <- raw %>% select(1:5)
-    colnames(raw) <- c("col1","col2", "col3", "col4", "col5")
-
-  }
-  
-  
     
-    #Adding in semester and year data
-    semester_year <- unlist(str_split(roster, pattern = "\\/"))[2]
-    file_semester <- str_extract(semester_year, pattern = "Fall|Winter|Spring|Summer")
-    file_year <- str_extract(semester_year, pattern = "\\d\\d\\d\\d")
+  #Adding in semester and year data
+  semester_year <- unlist(str_split(roster, pattern = "\\/"))[2]
+  file_semester <- str_extract(semester_year, pattern = "Fall|Winter|Spring|Summer")
+  file_year <- str_extract(semester_year, pattern = "\\d\\d\\d\\d")
     
-    raw <- raw %>% mutate(semester = str_to_upper(file_semester),
+  raw <- raw %>% mutate(semester = str_to_upper(file_semester),
                           year = str_to_upper(file_year))
+  
+  }
    
    #Putting everything together
    if (str_detect(roster, pattern = "Fall 2006")) {
@@ -75,6 +65,15 @@ for (roster in roster_files) {
 
 
 ## CODE GRAVEYARD ##
+
+#Anonymous/lambda function version
+#colnames(raw) <- map(y, function(x) paste0("col", y))
+
+#'Formula' version
+#colnames(raw) <- map(y, ~paste0("col", .x))
+
+
+
 # Although the intuition of converting a list to a dataframe to then use stringr works,
 # purrr provides the solutions for filtering without having to take extra steps and with
 # cleaner code. Hadley you da man!
