5f11b97a5c040709026383a0469b0c2cdb092371
danielsnyder116 <danielsnyder116@gmail.com>
2020-06-26 23:29:31 -0400

    Plottin' time

--- Data Analysis /Student-Data-Analysis.R
+++ Data Analysis /Student-Data-Analysis.R
@@ -1,21 +1,45 @@
 library(ggplot2)
+library(dplyr)
 
 setwd("/Users/Daniel/Desktop")
 data <- read.csv("student-class-blackbaud-input_2000-2019.csv")
 
-data <- data %>% mutate(semester=factor(semester, levels = c("FALL", "WINTER", "SPRING", "SUMMER")))
+glimpse(data)
 
-unique(data$semester)
+data <- data %>% mutate(semester=factor(semester, levels = c("WINTER", "SPRING", "SUMMER", "FALL")))
 
-by_sem_year <- data %>% group_by(semester, year) %>% summarize(test = count(unique(class_id)))
-                                                               
 
-seasons <- c("#F28E2B", "#4E79A7", "#59A14F", "#EDC948")
+#### NUMBER OF STUDENTS BY SEASON OVER TIME ####
 
-# NUMBER OF CLASSES BY SEASON OVER THE YEARS
-ggplot(data=by_sem_year, aes(x=year, y=num_classes, fill=semester)) + geom_point(shape=21, size=2) +
+#Each row is a unique student which is why just doing n() in summarize gives us that number
+student_count <- data %>% group_by(semester, year) %>% summarize(num_students=n())
+seasons <- c("#4E79A7", "#59A14F", "#EDC948","#F28E2B")
+ggplot(data=student_count, aes(x=year, y=num_students, fill=semester)) + geom_point(shape=21, size=2) +
   facet_grid(.~ semester) + stat_smooth(method="loess", span=.95, color="#76B7B2") +
   scale_fill_manual("Semester", values=seasons)
+
+
+#### TOTAL NUMBER OF MALE AND FEMALE STUDENTS BY SEASON OVER TIME ####
+#count(data, vars=gender)
+gender_ratio <- data %>% filter(!is.na(gender)) %>% group_by(semester, year, gender) %>%
+                    summarize(num_gender = n())
+                      
+gender_colors <- c("red", "blue")
+
+ggplot(data=gender_ratio, aes(x=year, y=num_gender, color=gender)) + geom_point(alpha=.5) +
+  stat_smooth(method = "loess", span = .95, se=TRUE, alpha=.3) + facet_grid(.~semester) +
+       scale_color_manual("Gender", values=gender_colors) 
+
+#Add in title, clean axis labels and add in more ticks
+
+
+# # Bar graph of gender over time - not the best way but interesting to make
+# ggplot(data=gender_ratio, aes(x=gender, y=num_gender, fill=gender)) + geom_col() +
+#   scale_fill_manual("Gender", values=gender_colors) + facet_grid(.~year) 
+#   #use facet_wrap when one wants to have multiple x axes
+
+#geom_bar - allow ggplot to count frequency for you
+#geom_col - set the value to compare (frequency, proportion, length, etc.)
 
 
 
